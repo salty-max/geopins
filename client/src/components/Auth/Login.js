72f9dde0db718a39b07a6c70a2dbd6987916368a
max-jellycat <max@jellycat.fr>
2019-08-20 16:33:12 +0200

    Google OAuth2 login

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,35 @@
 import React from "react";
+import { GraphQLClient } from 'graphql-request';
+import { GoogleLogin } from 'react-google-login';
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
+const ME_QUERY = `
+  {
+    me {
+      _id
+      name
+      email
+      picture
+    }
+  }
+`;
+
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const onSuccess = async googleUser => {
+    const idToken = googleUser.getAuthResponse().id_token
+    const client = new GraphQLClient('http://localhost:4000/graphql', {
+      headers: { authorization: idToken }
+    })
+    const data = await client.request(ME_QUERY)
+    console.log({ data })
+  }
+
+  return <GoogleLogin
+    clientId="963714159989-njd6kp0ha1cbb4mqs2l1pfmi71nlhm0j.apps.googleusercontent.com"
+    onSuccess={ onSuccess }
+    isSignedIn={ true }
+  />;
 };
 
 const styles = {
